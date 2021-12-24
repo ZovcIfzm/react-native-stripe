fa2b1a31ebba5f508a8c142216ad2751d9441eec
Alex Ruan <alexruan2222@gmail.com>
2021-12-23 22:50:21 -0500

    react native stripe init from tutorial
    https://blog.logrocket.com/exploring-the-new-stripe-react-native-sdk/

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import React from 'react';
+import {SafeAreaView, StyleSheet} from 'react-native';
+import {StripeProvider} from '@stripe/stripe-react-native';
+import PaymentScreen from './screens/paymentScreen';
+const App = () => {
+  const publishableKey = 'pk_test_AtN3VLAFhzbLNqf3Y9z50iNQ';
+  return (
+    <StripeProvider publishableKey={publishableKey}>
+      <PaymentScreen />
+    </StripeProvider>
+  );
+};
+const styles = StyleSheet.create({});
+export default App;
